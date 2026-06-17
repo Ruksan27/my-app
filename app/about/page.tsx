a6f0b582c003e51b756a191168b082ca7e6a2147
Ruksan Karki <ruksankarki80@gmail.com>
2026-06-17 09:40:24 +0545

    first day first code

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function iii() {
+    return (
+        <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            <h1 className="text-3xl font-bold mb-4">About Us</h1>
+            <p className="text-lg text-gray-600 dark:text-slate-300">
+                Welcome to our company! We are a team of passionate developers dedicated to creating amazing web applications.
+            </p>
+        </div>
+    ); 
+} 
